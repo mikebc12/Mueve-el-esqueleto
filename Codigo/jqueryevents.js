60a1cfc4608bfa23d176784410de88eb94f20d2e
mikebc12 <miguel.bugarin@rai.usc.es>
2024-03-19 10:42:59 +0100

    jQuery+jes6: función menu
    El menu interno de cada página se genera a partir de los títulos de sus artículos
    Se genera el href de su parte de la página: al clicar sobre un elemento del menú lleva a su subapartado
    Gestiones algo distintas según el tipo de página

--- Codigo/jqueryevents.js
+++ Codigo/jqueryevents.js
@@ -1,19 +1,45 @@
 //Label que indica el nivel seleccionado en la barra de los forms
-$(document).ready(function() {
-    $('#lvlbaile').on('input', function() {
-        switch (parseInt($(this).val())) {
+$(document).ready(() => {
+    $('#lvlbaile').on('input', () => {
+        switch (parseInt($('#lvlbaile').val())) {
             case 0:
                 $('#nivelTextoSpan').text("Básico");
-            break;
+                break;
             case 1:
                 $('#nivelTextoSpan').text("Medio");
-            break;
+                break;
             case 2:
                 $('#nivelTextoSpan').text("Avanzado");
-            break;
+                break;
             default:
                 $('#nivelTextoSpan').text("Desconocido");
-            break;
+                break;
+        }
+    });
+
+    //Obtenemos el título de cada artículo y rellenamos el menú de navegación de la página con ello 
+    $('article h2').each((index, element) => { //Miramos cada h2 de cada article
+        //obtenemos el texto sin formato
+        let text = $(element).text();
+        //Y la url actual para poder generar el href de cada subapartado.
+        let currentPageUrl = window.location.href;
+
+        //Tenemos tres casos:
+        if (document.title === "cursos_log" || document.title === "cursos_nolog") {
+            //Caso 1: página de cursos. Al hacer div en columnas tenemos que buscar el id en el nodo hijo de h2.
+            //Obtenemos el hijo e insertamos su id para el href
+            let child = element.firstChild;
+            let listItem = $('<li>').html(`<a href="${currentPageUrl}#${child.id}">${text}</a>`);
+            $('aside ul').append(listItem);
+        } else if (document.title === "usolibre_log" || document.title === "usolibre_nolog") {
+            //Caso 2: pagina de uso libre. Al implementar bootstrap tenemos que generar los elementos de una forma distinta.
+            let child = element.firstChild;
+            let listItem = $('<a>').attr('href', `${currentPageUrl}#${child.id}`).addClass('list-group-item list-group-item-action').text(text);
+            $('aside').append(listItem);         
+        }else {
+            //Caso 2: el id lo obtenemos directamente del elemento h2
+            let listItem = $('<li>').html(`<a href="${currentPageUrl}#${element.id}">${text}</a>`);
+            $('aside ul').append(listItem);
         }
     });
 });
